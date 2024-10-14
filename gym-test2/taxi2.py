22ffc6063087737c97c83f1d8cdae9e6e2714043
Lawrence Kesteloot <lk@teamten.com>
2024-10-13 21:18:17 -0700

    Taxi: Add replay buffer

--- gym-test2/taxi2.py
+++ gym-test2/taxi2.py
@@ -1,4 +1,5 @@
 
+import sys
 import os
 os.environ['KERAS_BACKEND'] = 'torch'
 
@@ -18,7 +19,13 @@ observation_count = env.observation_space.n
 action_count = env.action_space.n
 
 class Step:
-    def __init__(self, ):
+    def __init__(self, observation, action, reward, total_reward, episode_over, new_observation):
+        self.observation = observation
+        self.action = action
+        self.reward = reward
+        self.total_reward = total_reward
+        self.episode_over = episode_over
+        self.new_observation = new_observation
 
 class Playthrough:
     def __init__(self, steps, total_reward):
@@ -26,7 +33,7 @@ class Playthrough:
         self.total_reward = total_reward
 
 def make_one_hot(n, i):
-    return [int(i == j) for j in range(n)]
+    return np.array([int(i == j) for j in range(n)])
 
 def make_input_tensor(observation):
     # For Embedding:
@@ -45,11 +52,12 @@ def make_input_tensor(observation):
     ## return np.array([[taxi_row, taxi_rol, passenger_location, destination]])
 
     # Several one-hot arrays:
-    return np.array([
-        make_one_hot(5, taxi_row) +
-        make_one_hot(5, taxi_col) +
-        make_one_hot(5, passenger_location) +
-        make_one_hot(4, destination)])
+    return np.concatenate( (
+        make_one_hot(5, taxi_row),
+        make_one_hot(5, taxi_col),
+        make_one_hot(5, passenger_location),
+        make_one_hot(4, destination)
+    ) )
 
 def play(model, epsilon):
     steps = []
@@ -59,142 +67,126 @@ def play(model, epsilon):
 
     episode_over = False
     total_reward = 0
-    step = 0
     while not episode_over:
-        with tf.profiler.experimental.Trace("train", step_num=step, _r=1):
-            step += 1
-            old_input_tensor = make_input_tensor(observation)
+        old_input_tensor = make_input_tensor(observation)
 
-            action_mask = info["action_mask"]
-            model_prediction = None
-            if learn and random.random() < epsilon:
-                # Explore.
-                action = env.action_space.sample(action_mask)
-            else:
-                # Exploit.
-                actions = model.predict(old_input_tensor, verbose=0)[0]
-                model_prediction = actions
-                print("Choosing from", actions)
-                # print("Masks        ", action_mask)
-                action = max((actions[potential_action], potential_action)
-                             for potential_action in range(action_count)
-                             if action_mask[potential_action])[1]
+        action_mask = info["action_mask"]
+        if random.random() < epsilon:
+            # Explore.
+            action = env.action_space.sample(action_mask)
+        else:
+            # Exploit.
+            actions = model.predict(np.array([old_input_tensor]), verbose=0)[0]
+            #print("Choosing from", actions)
+            #print("Masks        ", action_mask)
+            action = max((actions[potential_action], potential_action)
+                         for potential_action in range(action_count)
+                         if action_mask[potential_action])[1]
 
-            new_observation, reward, terminated, truncated, info = env.step(action)
-            total_reward += reward
-            if False:
-                print("action", action,
-                      "new observation", new_observation,
-                      "reward", reward,
-                      "total reward", total_reward)
+        new_observation, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
+        if False:
+            print("action", action,
+                  "new observation", new_observation,
+                  "reward", reward,
+                  "total reward", total_reward)
 
-                print(env.render())
+            print(env.render())
 
-            episode_over = terminated or truncated
-            observation = new_observation
+        episode_over = terminated or truncated
+        steps.append(Step(observation, action, reward, total_reward, episode_over, new_observation))
+
+        observation = new_observation
 
     return Playthrough(steps, total_reward)
 
-def play(learn, model, alpha, gamma, epsilon):
-    observation, info = env.reset()
-    #print(observation, info)
+def gather_steps(model, epsilon, min_steps, min_reward):
+    steps = []
 
-    episode_over = False
+    print("Gathering steps", "epsilon =", epsilon)
+
+    game_count = 0
+    used_count = 0
     total_reward = 0
-    step = 0
-    while not episode_over:
-        with tf.profiler.experimental.Trace("train", step_num=step, _r=1):
-            step += 1
-            old_input_tensor = make_input_tensor(observation)
+    total_used_reward = 0
+    while len(steps) < min_steps:
+        playthrough = play(model, epsilon)
+        game_count += 1
+        total_reward += playthrough.total_reward
+        if playthrough.total_reward >= min_reward:
+            steps.extend(playthrough.steps)
+            used_count += 1
+            total_used_reward += playthrough.total_reward
 
-            action_mask = info["action_mask"]
-            model_prediction = None
-            if learn and random.random() < epsilon:
-                # Explore.
-                action = env.action_space.sample(action_mask)
-            else:
-                # Exploit.
-                actions = model.predict(old_input_tensor, verbose=0)[0]
-                model_prediction = actions
-                print("Choosing from", actions)
-                # print("Masks        ", action_mask)
-                action = max((actions[potential_action], potential_action)
-                             for potential_action in range(action_count)
-                             if action_mask[potential_action])[1]
+    print("    Used %d of %d games, %d reward, %d used reward" % (used_count, game_count,
+                                                                  total_reward/game_count,
+                                                                  total_used_reward/used_count))
 
-            new_observation, reward, terminated, truncated, info = env.step(action)
-            total_reward += reward
-            if False:
-                print("action", action,
-                      "new observation", new_observation,
-                      "reward", reward,
-                      "total reward", total_reward)
+    return steps
 
-                print(env.render())
+def learn(model, steps, gamma):
+    print("Learning", "gamma =", gamma)
 
-            if learn:
-                if False:
-                    W = model.get_weights()
-                    for i in range(len(W)):
-                        print(i, W[i].shape)
-                before = time.perf_counter()
-                new_input_tensor = make_input_tensor(new_observation)
-                futures = model.predict(new_input_tensor, verbose=0)#.numpy()
-                #print("timing 1", int((time.perf_counter() - before)*1000))
-                best_future = np.max(futures)
-                target = reward + gamma*best_future
-                before = time.perf_counter()
-                target_vector = model.predict(old_input_tensor, verbose=0)[0]
-                #print("timing 2", int((time.perf_counter() - before)*1000))
-                target_vector[action] = target
-                target_vector = target_vector.reshape(-1, env.action_space.n)
-                # print("target_vector", target_vector)
-                before = time.perf_counter()
-                model.fit(old_input_tensor, target_vector, epochs=1, verbose=0)
-                #print("timing 3", int((time.perf_counter() - before)*1000))
-                best_future = np.max(futures)
-                target = reward + gamma*best_future
+    old_input_tensor = np.array([make_input_tensor(step.observation) for step in steps])
+    new_input_tensor = np.array([make_input_tensor(step.new_observation) for step in steps])
+    action = np.array([step.action for step in steps])
+    reward = np.array([step.reward for step in steps])
 
-            episode_over = terminated or truncated
-            observation = new_observation
+    before = time.perf_counter()
+    futures = model.predict(new_input_tensor, verbose=0)
+    print("    timing 1", int((time.perf_counter() - before)*1000), "futures", futures.shape)
 
-    return total_reward
+    # Figure out target.
+    best_future = np.max(futures, axis=1)
+    target = reward + gamma*best_future
+
+    before = time.perf_counter()
+    target_vector = model.predict(old_input_tensor, verbose=0)
+    print("    timing 2", int((time.perf_counter() - before)*1000))
+
+    target_vector[np.arange(target_vector.shape[0]),action] = target
+    #target_vector = target_vector.reshape(-1, env.action_space.n)
+
+    before = time.perf_counter()
+    model.fit(old_input_tensor, target_vector, epochs=10, verbose=0)
+    print("    timing 3", int((time.perf_counter() - before)*1000))
 
 def main():
     print("Episode count [domain]\tTable")
-    input_tensor_size = make_input_tensor(0).shape[1]
-    for episode_count in range(1000, 2001, 100):
+    input_tensor_size = make_input_tensor(0).shape[-1]
+    for episode_count in range(10, 101, 10):
         model = Sequential([
             InputLayer( (input_tensor_size,) ),
-            #Embedding(observation_count, 4),
-            #Reshape((4,)),
             Dense(50, activation='relu'),
-            Dense(50, activation='relu'),
-            Dense(50, activation='relu'),
+            #Dense(50, activation='relu'),
+            #Dense(50, activation='relu'),
             Dense(action_count, activation='linear'),
         ])
         model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+        #model.compile(Adam(lr=1e-2), loss='mae')
         #model.build(make_input_tensor(0).shape)
-        print(model.summary())
+        model.summary()
 
         alpha_range = (0.9, 0.1)  # Learning rate.
         gamma = 0.99 # Discount factor.
-        epsilon_range = (0.9, 0.0) # Explore (exploit). 
+        epsilon_range = (1.0, 0.0) # Explore (exploit). 
 
         for episode in range(episode_count):
             alpha = alpha_range[0] + (alpha_range[1] - alpha_range[0])*episode/episode_count
             epsilon = epsilon_range[0] + (epsilon_range[1] - epsilon_range[0])*episode/episode_count
 
-            tf.profiler.experimental.start('logdir')
-            total_reward = play(True, model, alpha, gamma, epsilon)
-            tf.profiler.experimental.stop()
-            print("learn", episode, "/", episode_count, "=", total_reward)
-            return
+            steps = gather_steps(model, epsilon, 1000, -199)
+            learn(model, steps, gamma)
 
-        play_count = 1
+            #total_reward = play(True, model, alpha, gamma, epsilon)
+            #print("learn", episode, "/", episode_count, "=", total_reward)
+            #return
+
+        play_count = 10
         total_reward_total = 0
         for episode in range(play_count):
-            total_reward = play(False, model, None, None, None)
+            playthrough = play(model, 0)
+            total_reward = playthrough.total_reward
             print("play", episode, "/", play_count, "=", total_reward)
             total_reward_total += total_reward
         print(episode_count, total_reward_total / play_count)
