f6007d41e2af1fe8155ded0abac178835b8d321e
Lawrence Kesteloot <lk@teamten.com>
2024-10-24 15:16:23 -0700

    Normalize inputs, add command to make replay buffer.

--- dqn.py
+++ dqn.py
@@ -20,7 +20,7 @@ GAMMA = 0.99 # Discount factor.
 EPSILON_RANGE = (1.0, 0.1) # Explore (exploit). 
 EPSILON_RANGE = (0.0, 0.0) # Only exploit.
 EPISODE_EXPLORE_COUNT = int(EPISODE_COUNT*0.10) # How many episodes to interpolate over EPSILON_RANGE.
-REPLAY_BUFFER_SIZE = 100000
+REPLAY_BUFFER_SIZE = 1_000_000
 MINIBATCH_SIZE = 32 # How many replays to sample when back-propagating.
 TARGET_MODEL_UPDATE_PERIOD = 1000 # How often to update the target model.
 HISTORY_SIZE = 4 # Number of game states to include in NN inputs.
@@ -34,27 +34,35 @@ LOSS_FILENAME = "loss.txt"
 
 FOR_NOW = True
 
-def move_recommendation(game_state):
-    # Just whether to move left or right.
-    players = game_state.get_entities_by_types([game.TYPE_PLAYER_SHIP])
+# Enemy with highest Y value, or None if there are no enemies.
+def get_lowest_enemy(game_state):
     enemies = game_state.get_entities_by_types(game.TYPE_ENEMIES)
+    if enemies:
+        lowest_enemy = enemies[0]
+        for enemy in enemies:
+            if enemy.y > lowest_enemy.y:
+                lowest_enemy = enemy
+    else:
+        lowest_enemy = None
 
+    return lowest_enemy
+
+def get_player(game_state):
+    players = game_state.get_entities_by_types([game.TYPE_PLAYER_SHIP])
+    return players[0] if players else None
+
+def get_player_bullet(game_state):
+    player_bullets = game_state.get_entities_by_types([game.TYPE_PLAYER_SHOT])
+    return player_bullets[0] if player_bullets else None
+
+def move_recommendation(game_state):
     move = 0
-    if players:
-        if enemies:
-            lowest_enemy = enemies[0]
-            for enemy in enemies:
-                if enemy.y > lowest_enemy.y:
-                    lowest_enemy = enemy
-            target_x = lowest_enemy.x
-        else:
-            target_x = 64
 
-        px = players[0].x
-        if px < target_x:
-            move = 1
-        elif px > target_x:
-            move = -1
+    player = get_player(game_state)
+    if player:
+        lowest_enemy = get_lowest_enemy(game_state)
+        target_x = 64 if lowest_enemy is None else lowest_enemy.x
+        move = target_x - player.x
 
     return move
 
@@ -72,13 +80,36 @@ def moving_left(previous_game_state, next_game_state):
     # Peg to the left.
     return next_game_state.entities[0].x == 0
 
+def moving(previous_game_state, next_game_state):
+    return next_game_state.entities[0].x != previous_game_state.entities[0].x
+
+def normalize_x(x):
+    return x/64 - 1
+
+def normalize_y(y):
+    return y/24 - 1
+
+def unnormalize_x(x):
+    return round((x + 1)*64)
+
+def unnormalize_y(y):
+    return round((y + 1)*24)
+
+def normalized_x_of(entity):
+    return normalize_x(entity.x if entity else 64)
+
+def normalized_y_of(entity):
+    return normalize_y(entity.y if entity else 24)
+
 # Preprocessed sequence of game states.
 class Phi:
     def __init__(self, previous_phi, game_state):
         if previous_phi is None:
-            self.game_states = [game_state]
+            self.game_states = []
         else:
             self.game_states = previous_phi.game_states[-(HISTORY_SIZE - 1):]
+
+        while len(self.game_states) < HISTORY_SIZE:
             self.game_states.append(game_state)
 
     def input_tensor(self):
@@ -86,19 +117,43 @@ class Phi:
         # by the caller.
         game_state = self.game_states[-1]
 
-        if True:
+        # Fix data read from a file.
+        while len(self.game_states) < HISTORY_SIZE:
+            self.game_states.insert(0, self.game_states[0])
+
+        if False:
+            # Simple direction, left or right.
             return np.array([
                 move_recommendation(game_state),
             ])
 
-        if True:
-            # Just X position of player and one bad guy.
-            players = game_state.get_entities_by_types([game.TYPE_PLAYER_SHIP])
-            enemies = game_state.get_entities_by_types(game.TYPE_ENEMIES)
+        if False:
+            # Minimal info for all states.
+            x = []
+            for game_state in self.game_states:
+                player = get_player(game_state)
+                player_bullet = get_player_bullet(game_state)
+                lowest_enemy = get_lowest_enemy(game_state)
+                x.extend([
+                    normalized_x_of(player),
+                    normalized_x_of(player_bullet),
+                    normalized_y_of(player_bullet),
+                    normalized_x_of(lowest_enemy),
+                    normalized_y_of(lowest_enemy),
+                ])
+            return np.array(x)
 
+        if True:
+            # Minimal info.
+            player = get_player(game_state)
+            player_bullet = get_player_bullet(game_state)
+            lowest_enemy = get_lowest_enemy(game_state)
             return np.array([
-                players[0].x if players else 0,
-                enemies[0].x if enemies else 0,
+                normalized_x_of(player),
+                normalized_x_of(player_bullet),
+                normalized_y_of(player_bullet),
+                normalized_x_of(lowest_enemy),
+                normalized_y_of(lowest_enemy),
             ])
 
         if False:
@@ -137,23 +192,23 @@ class Transition:
         self.next_phi = next_phi
 
     def compute_reward(self):
-        next_game_state = self.next_phi.game_states[-1]
         previous_game_state = self.previous_phi.game_states[-1]
+        next_game_state = self.next_phi.game_states[-1]
 
-        if False:
+        if True:
             # Score.
             # TODO take into account number of gas cans.
             # TODO take into account 16-bit score wrapping.
             reward = next_game_state.score - previous_game_state.score
 
-        if True:
+        if False:
             rec = move_recommendation(previous_game_state)
             if rec < 0:
                 reward = int(moving_left(previous_game_state, next_game_state))
             elif rec > 0:
                 reward = int(moving_right(previous_game_state, next_game_state))
             else:
-                reward = 0
+                reward = int(not moving(previous_game_state, next_game_state))
 
         if False:
             if previous_game_state.get_fuel_can_count() > 8:
@@ -182,25 +237,51 @@ class Transition:
         return "Transition[%s, \"%s\", %d, %s]" % \
                 (self.previous_phi, game.ACTIONS[self.action], self.compute_reward(), self.next_phi)
 
+class ReplayBuffer:
+    def __init__(self, size):
+        self.buffer = deque([], size)
+
+    def sample(self, count):
+        return random.sample(self.buffer, min(count, len(self.buffer)))
+
+    def add(self, transition):
+        self.buffer.append(transition)
+
+    def load(self):
+        # Replay buffer of Transition objects.
+        if os.path.exists(REPLAY_BUFFER_FILENAME):
+            with open(REPLAY_BUFFER_FILENAME, "rb") as f:
+                new_transitions = pickle.load(f)
+                self.buffer.extend(new_transitions)
+            print("Loaded replay buffer:", len(new_transitions))
+        else:
+            print("Replay buffer missing")
+
+    def save(self):
+        with open(REPLAY_BUFFER_FILENAME, "wb") as f:
+            pickle.dump(self.buffer, f)
+
 class DQN:
     def __init__(self, input_size, output_size):
         self.input_size = input_size
         self.output_size = output_size
 
-        self.replay_buffer = deque([], REPLAY_BUFFER_SIZE)
+        self.replay_buffer = ReplayBuffer(REPLAY_BUFFER_SIZE)
 
         # Average of input and output.
         middle_layer_size = (input_size + output_size) // 2
-        #middle_layer_size = 1
+        middle_layer_size = input_size # Guaranteed not lossy.
+        middle_layer_size = 32
 
         # Create neural net.
         self.model = Sequential([
             InputLayer( (input_size,) ),
             # relu = rectified linear unit = max(0, x)
-            #Dense(middle_layer_size, activation='relu'),
+            Dense(middle_layer_size, activation='relu'),
+            Dense(middle_layer_size, activation='relu'),
             #Dense(middle_layer_size, activation='relu', kernel_initializer="zeros", bias_initializer="zeros"),
-            #Dense(output_size, activation='linear'),
-            Dense(output_size, activation='linear', kernel_initializer="zeros", bias_initializer="zeros"),
+            Dense(output_size, activation='linear'),
+            #Dense(output_size, activation='linear', kernel_initializer="zeros", bias_initializer="zeros"),
         ])
         self.model.compile(loss='mse', optimizer=Adam(), metrics=['mae'])
         self.model.summary()
@@ -218,7 +299,7 @@ class DQN:
     def update(self):
         # Sample minibatch of transitions from replay buffer.
         # TODO: Sample proportionally to loss.
-        transitions = random.sample(self.replay_buffer, min(MINIBATCH_SIZE, len(self.replay_buffer)))
+        transitions = self.replay_buffer.sample(MINIBATCH_SIZE)
 
         # Compute predictions of batch.
         previous_input_tensors = np.array([transition.previous_phi.input_tensor()
@@ -270,20 +351,13 @@ class DQN:
     def take_target_model_snapshot(self):
         self.target_model = clone_model(self.model)
 
-    def load_replay_buffer(self):
-        # Replay buffer of Transition objects.
-        if os.path.exists(REPLAY_BUFFER_FILENAME):
-            with open(REPLAY_BUFFER_FILENAME, "rb") as f:
-                new_transitions = pickle.load(f)
-                self.replay_buffer.extend(new_transitions)
-            print("Loaded replay buffer:", len(new_transitions))
-
-    def dump_replay_buffer(self):
-        before = time.perf_counter()
-        with open(REPLAY_BUFFER_FILENAME, "wb") as f:
-            pickle.dump(self.replay_buffer, f)
-        after = time.perf_counter()
-        print("dqn dump time", int((after - before)*1000), "ms")
+    def print_weights(self):
+        for i, layer in enumerate(self.model.layers):
+            print("Weights for layer %d:" % (i + 1,))
+            weights, biases = layer.get_weights()
+            print(weights)
+            print(biases)
+            print()
 
 # Compute the box (straight) average of the previous "size" numbers.
 class BoxAverage:
@@ -303,11 +377,15 @@ class BoxAverage:
 def learn():
     # Parameters of the neural network.
     input_tensor_shape = Phi(None, game.GameState()).input_tensor().shape
+    print("phi", Phi(None, game.GameState()))
+    print("input_tensor", Phi(None, game.GameState()).input_tensor())
     print("input_tensor_shape", input_tensor_shape)
     action_count = len(game.ACTIONS)
 
     dqn = DQN(input_tensor_shape[0], action_count)
-    dqn.load_replay_buffer()
+    print("Initial weights:")
+    dqn.print_weights()
+    dqn.replay_buffer.load()
 
     if False and os.path.exists(WEIGHTS_FILENAME):
         print("Loading weights from " + WEIGHTS_FILENAME)
@@ -326,7 +404,7 @@ def learn():
                                    "\t".join(action if action != "" else "None"
                                              for action in game.ACTIONS) + "\n")
             loss_file.write("Step [domain]\tLoss [zero]\tMAE [right,zero]\n")
-            while learn_step < 1_000_000:
+            while True:
                 loss, mae, predictions = dqn.update()
                 if learn_step % 1000 == 0:
                     predictions_file.write(str(learn_step) + " " +
@@ -343,7 +421,8 @@ def learn():
                 if learn_step % TARGET_MODEL_UPDATE_PERIOD == 0:
                     dqn.take_target_model_snapshot()
                 if learn_step % 1000 == 0:
-                    print("    %d" % (learn_step,))
+                    print("%d" % (learn_step,))
+                    dqn.print_weights()
                 if learn_step % 1000 == 0:
                     print("Saving weights to " + WEIGHTS_FILENAME)
                     dqn.model.save_weights(WEIGHTS_FILENAME)
@@ -361,7 +440,7 @@ def learn():
         now = time.perf_counter()
         epsilon = EPSILON_RANGE[0] + (EPSILON_RANGE[1] - EPSILON_RANGE[0])*min(episode/EPISODE_EXPLORE_COUNT, 1)
         estimated_minutes_left = 0 if episode == 0 else EPISODE_COUNT/episode*(now - start_time)/60
-        print("episode", episode, "step", step, "epsilon", epsilon, "replay buffer", len(dqn.replay_buffer), "time", now, "minutes left", int(estimated_minutes_left))
+        print("episode", episode, "step", step, "epsilon", epsilon, "replay buffer", len(dqn.replay_buffer.buffer), "time", now, "minutes left", int(estimated_minutes_left))
 
         # Run the game.
         seed = episode
@@ -414,7 +493,7 @@ def learn():
             phi = next_phi
 
             # Append transition to replay buffer.
-            dqn.replay_buffer.append(transition)
+            dqn.replay_buffer.add(transition)
             #print(transition, phi.game_states[-1].entities[0].x)
 
             if UPDATE_PERIOD is not None and step % UPDATE_PERIOD == 0:
@@ -430,7 +509,7 @@ def learn():
 
         if False:
             # Save the replay buffer between every game.
-            dqn.dump_replay_buffer()
+            dqn.replay_buffer.save()
 
     live_game.kill()
 
@@ -441,11 +520,13 @@ def play():
     action_count = len(game.ACTIONS)
 
     dqn = DQN(input_tensor_shape[0], action_count)
-    dqn.load_replay_buffer()
+    dqn.replay_buffer.load()
 
     if os.path.exists(WEIGHTS_FILENAME):
         print("Loading weights from " + WEIGHTS_FILENAME)
         dqn.model.load_weights(WEIGHTS_FILENAME)
+
+    dqn.print_weights()
 
     # TODO I think the Lua version has a single loop of steps and they
     # restart the game when it finishes.
@@ -477,9 +558,9 @@ def play():
             if FOR_NOW and action < 3:
                 # Force fire.
                 action += 3
-            if FOR_NOW and game_step < 100:
-                # Move right to avoid initial death.
-                action = 2
+            if FOR_NOW and game_step < 120:
+                # Move left to avoid initial death.
+                action = 1
             print(input_tensor, actions, action, game.ACTIONS[action])
 
             # Execute action in game, get next state.
@@ -502,10 +583,110 @@ def play():
 
     live_game.kill()
 
+def print_transition(transition):
+    p = transition.previous_phi
+    n = transition.next_phi
+
+    def unnormalize(v):
+        return np.array([
+            unnormalize_x(v[0]),
+            unnormalize_x(v[1]),
+            unnormalize_y(v[2]),
+            unnormalize_x(v[3]),
+            unnormalize_y(v[4]),
+        ])
+
+    print(game.ACTIONS[transition.action], transition.compute_reward(), n.game_states[-1].game_over)
+    print(np.vstack([
+        unnormalize(p.input_tensor()[-5:]),
+        unnormalize(n.input_tensor()[-5:]),
+    ]))
+
+# Create and save a replay buffer from random actions.
+def make_replay():
+    DEBUG = False
+
+    replay_buffer = ReplayBuffer(REPLAY_BUFFER_SIZE)
+
+    start_time = time.perf_counter()
+    live_game = game.LiveGame(not DEBUG)
+    episode = 0
+
+    while len(replay_buffer.buffer) < REPLAY_BUFFER_SIZE:
+        now = time.perf_counter()
+        elapsed = now - start_time
+        size = len(replay_buffer.buffer)
+        num_left = max(0, REPLAY_BUFFER_SIZE - size)
+        estimated_minutes_left = 0 if size == 0 else elapsed/size*num_left/60
+
+        print("replay buffer", len(replay_buffer.buffer), "minutes left", int(estimated_minutes_left))
+
+        # Run the game.
+        seed = episode
+        game_state = live_game.start_new_game(seed)
+        game_step = 0
+
+        # Initialize our game state.
+        phi = Phi(None, game_state)
+
+        # Each iteration is an action chosen in the game.
+        while True:
+            if DEBUG:
+                time.sleep(0.050)
+
+            # Move randomly.
+            action = random.randrange(0, len(game.ACTIONS))
+            if game_step < 120 and action >= 3:
+                # Don't shoot to avoid initial death.
+                action -= 3
+
+            # Execute action in game, get next state.
+            game_state = live_game.perform_action(action)
+            next_phi = Phi(phi, game_state)
+
+            # Make transition to capture what we experienced.
+            transition = Transition(phi, action, next_phi)
+            if DEBUG:
+                print_transition(transition)
+            phi = next_phi
+
+            # Append transition to replay buffer.
+            replay_buffer.add(transition)
+
+            if game_state.game_over:
+                time.sleep(5)
+                print("Game over, score =", game_state.score)
+                break
+
+            game_step += 1
+
+        # Save the replay buffer between every game.
+        replay_buffer.save()
+
+        episode += 1
+
+    live_game.kill()
+
+# Analyze the saved replay buffer.
+def look_replay():
+    replay_buffer = ReplayBuffer(REPLAY_BUFFER_SIZE)
+    replay_buffer.load()
+
+    for transition in replay_buffer.buffer:
+        print_transition(transition)
+
 try:
-    if "--play" in sys.argv:
+    command = sys.argv[1] if len(sys.argv) == 2 else ""
+    if command == "play":
         play()
-    else:
+    elif command == "learn":
         learn()
+    elif command == "make-replay":
+        make_replay()
+    elif command == "look-replay":
+        look_replay()
+    else:
+        print("Unknown command")
 except KeyboardInterrupt:
     pass
+
